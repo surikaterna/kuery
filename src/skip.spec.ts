c43e3d553607824c7893f9bb2c01905eb4af34c1
Matteus Westman <matteus.westman@surikat.se>
2022-07-29 13:34:10 +0200

    fix: fixed skip test

--- src/skip.spec.ts
+++ src/skip.spec.ts
@@ -1,9 +1,8 @@
-var _ = require('lodash/fp');
-var Kuery = require('..');
-var should = require('should');
+import _ from 'lodash/fp';
+import Kuery from '.';
 
 
-var collection = [
+const collection = [
   {
     id: 1, name: 'Andreas', address: { street: 'Bellmansgatan' },
     born: new Date('1980-01-01T12:00:00.000Z')
@@ -19,65 +18,58 @@ var collection = [
 describe('Kuery', function () {
   describe('#skip', function () {
     it('should skip 2 documents', function () {
-      var r;
       var q = new Kuery({});
       q.skip(2);
-      r = q.find(collection);
-      r.length.should.equal(2);
-      r[0].id.should.equal(3);
+      const r = q.find(collection);
+      expect(r.length).toEqual(2);
+      expect(r[0].id).toEqual(3);
     });
     it('should skip 2 documents with query', function () {
-      var r;
       var q = new Kuery({ id: { $gt: 1 } });
       q.skip(2);
-      r = q.find(collection);
-      r.length.should.equal(1);
-      r[0].id.should.equal(4);
+      const r = q.find(collection);
+      expect(r.length).toEqual(1);
+      expect(r[0].id).toEqual(4);
     });
   });
   describe('#limit', function () {
     it('should limit 2 documents', function () {
-      var r;
       var q = new Kuery({});
       q.limit(2);
-      r = q.find(collection);
-      r.length.should.equal(2);
-      r[0].id.should.equal(1);
+      const r = q.find(collection);
+      expect(r.length).toEqual(2);
+      expect(r[0].id).toEqual(1);
     });
     it('should limit 2 documents with query', function () {
-      var r;
       var q = new Kuery({ id: { $gt: 1 } });
       q.limit(2);
-      r = q.find(collection);
-      r.length.should.equal(2);
-      r[0].id.should.equal(2);
+      const r = q.find(collection);
+      expect(r.length).toEqual(2);
+      expect(r[0].id).toEqual(2);
     });
     it('should limit 2, skip 1 documents with query', function () {
-      var r;
       var q = new Kuery({ id: { $gt: 1 } });
       q.limit(2).skip(1);
-      r = q.find(collection);
-      r.length.should.equal(2);
-      r[0].id.should.equal(3);
+      const r = q.find(collection);
+      expect(r.length).toEqual(2);
+      expect(r[0].id).toEqual(3);
     });
   });
   describe('#sort', function () {
     it('should sort on one property', function () {
-      var r;
       var q = new Kuery({});
       q.sort({born: 1});
-      r = q.find(collection);
-      r.length.should.equal(collection.length);
-      r[0].born.should.equal(collection[0].born);
-      r[1].born.should.equal(collection[3].born);
+      const r = q.find(collection);
+      expect(r.length).toEqual(collection.length);
+      expect(r[0].born).toEqual(collection[0].born);
+      expect(r[1].born).toEqual(collection[3].born);
     });
     it('should sort ands skip', function () {
-      var r;
       var q = new Kuery({});
       q.sort({born: -1});
       q.skip(1);
-      r = q.find(collection);
-      r[0].name.should.equal('Sven');
+      const r = q.find(collection);
+      expect(r[0].name).toEqual('Sven');
     });
   });
 });

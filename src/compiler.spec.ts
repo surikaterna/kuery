e0b3a103f2810c2544911df0ce010dbed20c62af
Matteus Westman <matteus.westman@surikat.se>
2022-07-29 13:35:14 +0200

    fix: fixed compiler test

--- src/compiler.spec.ts
+++ src/compiler.spec.ts
@@ -1,30 +1,37 @@
-var should = require('should');
+import _ from 'lodash/fp';
+import QueryCompiler from './compiler';
 
-var QueryCompiler = require('../lib/compiler');
 describe('QueryCompiler', function () {
   describe('compile', function () {
     it('compile ne null query', function () {
-      var query = {
+      const query = {
         attachmentId: { $ne: null }
       };
-      var compiler = new QueryCompiler();
-      should.doesNotThrow(function () {
+      const compiler = new QueryCompiler();
+
+      expect(() => {
         compiler.compile(query);
-      });
+      }).not.toThrowError();
+
+      // should.doesNotThrow(function () {
+      //   compiler.compile(query);
+      // });
     });
   });
   describe('_compilePredicates', function () {
     it('compile eq query', function () {
-      var compiler = new QueryCompiler();
-      var p = compiler._compilePredicates({age:10});
-      p[0]({age:10}).should.be.true;
+      const compiler = new QueryCompiler();
+      const p = compiler._compilePredicates({age:10});
+      expect(p[0]({age:10})).toBe(true);
+      // p[0]({age:10}).should.be.true;
     })
   });
   describe('_subQuery', function () {
     it('compile eq query', function () {
-      var compiler = new QueryCompiler();
-      var p = compiler._subQuery([{age:10}]);
-      p[0]({age:10}).should.be.true;
+      const compiler = new QueryCompiler();
+      const p = compiler._subQuery([{age:10}]);
+      expect(p[0]({age:10})).toBe(true);
+      // p[0]({age:10}).should.be.true;
     })
   });
 });

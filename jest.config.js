f9f2b250650cd36a8e34af3447e29cda6f1b13ae
Matteus Westman <matteus.westman@surikat.se>
2022-07-29 12:58:57 +0200

    feat: created `jest.config.js`

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    testMatch: [
+      "**/test/**/*.[jt]s?(x)",
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+    testTimeout: 2000,
+};

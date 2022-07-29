ff8bc7f2aa3c933a0c568ec88e459fd1d0b28f1b
Matteus Westman <matteus.westman@surikat.se>
2022-07-29 13:35:27 +0200

    fix: fixed jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    clearMocks: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-    testMatch: [
-      "**/test/**/*.[jt]s?(x)",
-      "**/__tests__/**/*.[jt]s?(x)",
-      "**/?(*.)+(spec|test).[jt]s?(x)"
-    ],
-    testTimeout: 2000,
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest']
+  },
+  testRegex: '((\\.|/)(test|spec))\\.ts$',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };

ca85120147474f896721f88b131e2d21b55b00c1
Matteus Westman <matteus.westman@surikat.se>
2022-07-29 13:26:18 +0200

    fix: fixed typings on `hidash`

--- src/hidash.ts
+++ src/hidash.ts
@@ -1,19 +1,21 @@
-var _ = require('lodash/fp');
-var __ = require('lodash');
+import _ from "lodash/fp";
+import __ from "lodash";
 
-var hi = {
-  __logN: function (name) {
-    return function (v) {
+const hi = {
+  __logN: function (name: any) {
+    return function (v: any) {
+      // @ts-ignore
       console.log('>>(' + name + ') ', v);
       return v;
-    };
+    }
   },
-  __log: function (v) {
+  __log: function (v: any) {
+    // @ts-ignore
     console.log('>>', v);
     return v;
   },
-  or: function OR(predicates) {
-    return function (v) {
+  or: function OR(predicates: any) {
+    return function (v: any) {
       var i;
       for (i = 0; i < predicates.length; i++) {
         if (predicates[i](v)) {
@@ -23,8 +25,8 @@ var hi = {
       return false;
     };
   },
-  and: function AND(predicates) {
-    return function (v) {
+  and: function AND(predicates: any) {
+    return function (v: any) {
       var i;
       for (i = 0; i < predicates.length; i++) {
         if (!predicates[i](v)) {
@@ -34,36 +36,36 @@ var hi = {
       return true;
     };
   },
-  check: function check(key, op) {
+  check: function check(key: any, op: any) {
     var res;
     if (key.indexOf('.') !== -1) {
-      res = function (v) {
+      res = function (v: any) {
         return _.some(op)(hi.collect(key)(v));
       };
     } else {
-      res = function (v) {
+      res = function (v: any) {
         return op(v[key]);
       };
     }
     return res;
   },
-  compare: function COMP(key, op, arg) {
+  compare: function COMP(key: any, op: any, arg: any) {
     return hi.check(key, op(_, arg));
   },
-  exists: function exists(key, op) {
-    return hi.check(key, function (v) {
+  exists: function exists(key: any, op: any) {
+    return hi.check(key, function (v: any) {
       return op ? !!v : !v;
     });
   },
-  collect: function collect(key) {
-    return function (v) {
+  collect: function collect(key: any) {
+    return function (v: any) {
       var path = key.split('.');
-      var res = [];
+      var res: any[] = [];
       hi._collect(res, v, path);
       return res;
     };
   },
-  _collect: function _collect(result, object, path) {
+  _collect: function _collect(result: any, object: any, path: any) {
     var index = 0;
     var length = path.length;
     var element = object;
@@ -85,6 +87,6 @@ var hi = {
       }
     }
   }
-};
+}
 
-module.exports = hi;
+export default hi;
